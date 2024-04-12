870770e040b51c134d87d54cc80b9ed8c23afc72
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-04-12 12:21:40 +0300

    remove client

--- clients/ts/accounts/index.ts
+++
@@ -1,4 +0,0 @@
-export { Registrar } from "./Registrar"
-export type { RegistrarFields, RegistrarJSON } from "./Registrar"
-export { Voter } from "./Voter"
-export type { VoterFields, VoterJSON } from "./Voter"
