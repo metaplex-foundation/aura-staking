e10066ec42dde2e3e1fea65caa6f448414f8ce2c
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-05-28 17:39:39 +0300

    extend CPI enum && provide source with program ids

--- programs/voter-stake-registry/src/cpi_instructions.rs
+++ programs/voter-stake-registry/src/cpi_instructions.rs
@@ -7,14 +7,51 @@ use solana_program::{
     entrypoint::ProgramResult,
     instruction::{AccountMeta, Instruction},
     program::{invoke, invoke_signed},
-    system_program, sysvar,
+    system_program,
 };
 
-// TODO: move the const
-pub const REWARD_CONTRACT_ID: Pubkey = solana_program::pubkey!("11111111111111111111111111111111");
+pub const REWARD_CONTRACT_ID: Pubkey =
+    solana_program::pubkey!("J8oa8UUJBydrTKtCdkvwmQQ27ZFDq54zAxWJY5Ey72Ji");
 
 #[derive(Debug, AnchorSerialize, AnchorDeserialize, PartialEq, Eq)]
 pub enum RewardsInstruction {
+    /// Creates and initializes a reward pool account
+    ///
+    /// Accounts:
+    /// [R] Root account (ex-Config program account)
+    /// [W] Reward pool account
+    /// [R] Deposit authority
+    /// [WS] Payer
+    /// [R] System program
+    InitializePool,
+
+    /// Creates a new vault account and adds it to the reward pool
+    ///
+    /// Accounts:
+    /// [R] Root account (ex-Config program account)
+    /// [W] Reward pool account
+    /// [R] Reward mint account
+    /// [W] Vault account
+    /// [WS] Payer
+    /// [R] Token program
+    /// [R] System program
+    /// [R] Rent sysvar
+    AddVault,
+
+    /// Fills the reward pool with rewards
+    ///
+    /// Accounts:
+    /// [W] Reward pool account
+    /// [R] Mint of rewards account
+    /// [W] Vault for rewards account
+    /// [RS] Transfer  account
+    /// [W] From account
+    /// [R] Token program
+    FillVault {
+        /// Amount to fill
+        amount: u64,
+    },
+
     /// Initializes mining account for the specified user
     ///
     /// Accounts:
@@ -63,6 +100,14 @@ pub enum RewardsInstruction {
     /// [W] User reward token account
     /// [R] Token program
     Claim,
+
+    /// Creates and initializes a reward root
+    ///
+    /// Accounts:
+    /// [WS] Root account (ex-Config program account)
+    /// [WS] Authority
+    /// [R] System program
+    InitializeRoot,
 
     /// Restakes deposit
     ///
