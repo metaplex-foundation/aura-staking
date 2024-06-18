e22328dedb5f536ac9d4579a2784978e1816dbb9
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-06-18 18:26:55 +0300

    make tests green

--- programs/voter-stake-registry/tests/test_all_deposits.rs
+++ programs/voter-stake-registry/tests/test_all_deposits.rs
@@ -1,165 +1,176 @@
-// use anchor_spl::token::TokenAccount;
-// use program_test::*;
-// use solana_program_test::*;
-// use solana_sdk::{signature::Keypair, signer::Signer, transport::TransportError};
-// use voter_stake_registry::state::{LockupKind, LockupPeriod};
+use anchor_spl::token::TokenAccount;
+use program_test::*;
+use solana_program_test::*;
+use solana_sdk::{signature::Keypair, signer::Signer, transport::TransportError};
+use voter_stake_registry::state::{LockupKind, LockupPeriod};
 
-// mod program_test;
-// #[tokio::test]
-// async fn test_all_deposits() -> Result<(), TransportError> {
-//     let context = TestContext::new().await;
-//     let addin = &context.addin;
+mod program_test;
+#[tokio::test]
+async fn test_all_deposits() -> Result<(), TransportError> {
+    let context = TestContext::new().await;
+    let addin = &context.addin;
 
-//     let payer = &context.users[0].key;
-//     let realm_authority = Keypair::new();
-//     let realm = context
-//         .governance
-//         .create_realm(
-//             "testrealm",
-//             realm_authority.pubkey(),
-//             &context.mints[0],
-//             payer,
-//             &context.addin.program_id,
-//         )
-//         .await;
+    let payer = &context.users[0].key;
+    let realm_authority = Keypair::new();
+    let realm = context
+        .governance
+        .create_realm(
+            "testrealm",
+            realm_authority.pubkey(),
+            &context.mints[0],
+            payer,
+            &context.addin.program_id,
+        )
+        .await;
 
-//     let voter_authority = &context.users[1].key;
-//     let voter_mngo = context.users[1].token_accounts[0];
-//     let token_owner_record = realm
-//         .create_token_owner_record(voter_authority.pubkey(), payer)
-//         .await;
+    let voter_authority = &context.users[1].key;
+    let voter_mngo = context.users[1].token_accounts[0];
+    let token_owner_record = realm
+        .create_token_owner_record(voter_authority.pubkey(), payer)
+        .await;
 
-//     let fill_authority = Keypair::from_bytes(&context.users[3].key.to_bytes()).unwrap();
-//     let distribution_authority = Keypair::new();
-//     let (registrar, rewards_pool) = context
-//         .addin
-//         .create_registrar(
-//             &realm,
-//             &realm_authority,
-//             payer,
-//             &fill_authority.pubkey(),
-//             &distribution_authority.pubkey(),
-//             &context.rewards.program_id,
-//         )
-//         .await;
-//     let mngo_voting_mint = addin
-//         .configure_voting_mint(
-//             &registrar,
-//             &realm_authority,
-//             payer,
-//             0,
-//             &context.mints[0],
-//             0,
-//             1.0,
-//             0.0,
-//             5 * 365 * 24 * 60 * 60,
-//             None,
-//             None,
-//         )
-//         .await;
+    let fill_authority = Keypair::from_bytes(&context.users[3].key.to_bytes()).unwrap();
+    let distribution_authority = Keypair::new();
+    let (registrar, rewards_pool) = context
+        .addin
+        .create_registrar(
+            &realm,
+            &realm_authority,
+            payer,
+            &fill_authority.pubkey(),
+            &distribution_authority.pubkey(),
+            &context.rewards.program_id,
+        )
+        .await;
+    let mngo_voting_mint = addin
+        .configure_voting_mint(
+            &registrar,
+            &realm_authority,
+            payer,
+            0,
+            &context.mints[0],
+            0,
+            1.0,
+            0.0,
+            5 * 365 * 24 * 60 * 60,
+            None,
+            None,
+        )
+        .await;
 
-//     let deposit_mining = find_deposit_mining_addr(
-//         &voter_authority.pubkey(),
-//         &rewards_pool,
-//         &context.rewards.program_id,
-//     );
+    let deposit_mining = find_deposit_mining_addr(
+        &voter_authority.pubkey(),
+        &rewards_pool,
+        &context.rewards.program_id,
+    );
 
-//     let voter = addin
-//         .create_voter(
-//             &registrar,
-//             &token_owner_record,
-//             voter_authority,
-//             payer,
-//             &rewards_pool,
-//             &deposit_mining,
-//             &context.rewards.program_id,
-//         )
-//         .await;
+    let voter = addin
+        .create_voter(
+            &registrar,
+            &token_owner_record,
+            voter_authority,
+            payer,
+            &rewards_pool,
+            &deposit_mining,
+            &context.rewards.program_id,
+        )
+        .await;
 
-//     for i in 0..32 {
-//         addin
-//             .create_deposit_entry(
-//                 &registrar,
-//                 &voter,
-//                 voter_authority,
-//                 &mngo_voting_mint,
-//                 i,
-//                 LockupKind::Constant,
-//                 LockupPeriod::ThreeMonths,
-//             )
-//             .await
-//             .unwrap();
-//         addin
-//             .deposit(
-//                 &registrar,
-//                 &voter,
-//                 &mngo_voting_mint,
-//                 voter_authority,
-//                 voter_mngo,
-//                 i,
-//                 12000,
-//                 &rewards_pool,
-//                 &deposit_mining,
-//                 &context.rewards.program_id,
-//             )
-//             .await
-//             .unwrap();
+    addin
+        .create_deposit_entry(
+            &registrar,
+            &voter,
+            voter_authority,
+            &mngo_voting_mint,
+            0,
+            LockupKind::None,
+            LockupPeriod::None,
+        )
+        .await
+        .unwrap();
+    addin
+        .deposit(
+            &registrar,
+            &voter,
+            &mngo_voting_mint,
+            voter_authority,
+            voter_mngo,
+            0,
+            32000,
+        )
+        .await
+        .unwrap();
 
-//         addin.lock_tokens(
-//             &registrar,
-//             &voter,
-//             authority,
-//             source_deposit_entry_index,
-//             target_deposit_entry_index,
-//             amount,
-//             realm_governing_mint_pubkey,
-//             realm_pubkey,
-//             &deposit_mining,
-//             rewards_program,
-//         )
-//     }
+    for i in 1..32 {
+        addin
+            .create_deposit_entry(
+                &registrar,
+                &voter,
+                voter_authority,
+                &mngo_voting_mint,
+                i,
+                LockupKind::Constant,
+                LockupPeriod::ThreeMonths,
+            )
+            .await
+            .unwrap();
+        addin
+            .lock_tokens(
+                &registrar,
+                &voter,
+                voter_authority,
+                &deposit_mining,
+                &context.rewards.program_id,
+                0,
+                i,
+                1000,
+                mngo_voting_mint.mint.pubkey.unwrap(),
+                realm.realm,
+            )
+            .await?;
+    }
 
-//     // advance time, to be in the middle of all deposit lockups
-//     advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 45 * 86400).await;
+    // advance time, to be in the middle of all deposit lockups
+    advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 45 * 86400).await;
 
-//     // the two most expensive calls which scale with number of deposits
-//     // are update_voter_weight_record and withdraw - both compute the vote weight
+    // the two most expensive calls which scale with number of deposits
+    // are update_voter_weight_record and withdraw - both compute the vote weight
 
-//     let vwr = addin
-//         .update_voter_weight_record(&registrar, &voter)
-//         .await
-//         .unwrap();
-//     assert_eq!(vwr.voter_weight, 12000 * 32);
+    let vwr = addin
+        .update_voter_weight_record(&registrar, &voter)
+        .await
+        .unwrap();
+    assert_eq!(vwr.voter_weight, 1000 * 32);
 
-//     advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 50 * 86400).await;
+    advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 50 * 86400).await;
 
-//     context
-//         .addin
-//         .unlock_tokens(&registrar, &voter, voter_authority, 0)
-//         .await
-//         .unwrap();
+    context
+        .addin
+        .unlock_tokens(&registrar, &voter, voter_authority, 0)
+        .await
+        .unwrap();
 
-//     advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 5 * 86400).await;
+    advance_clock_by_ts(&mut context.solana.context.borrow_mut(), 5 * 86400).await;
 
-//     // make sure withdrawing works with all deposits filled
-//     addin
-//         .withdraw(
-//             &registrar,
-//             &voter,
-//             &mngo_voting_mint,
-//             voter_authority,
-//             voter_mngo,
-//             0,
-//             1000,
-//             &rewards_pool,
-//             &deposit_mining,
-//             &context.rewards.program_id,
-//         )
-//         .await
-//         .unwrap();
+    // make sure withdrawing works with all deposits filled
+    addin
+        .withdraw(
+            &registrar,
+            &voter,
+            &mngo_voting_mint,
+            voter_authority,
+            voter_mngo,
+            0,
+            1000,
+            &rewards_pool,
+            &deposit_mining,
+            &context.rewards.program_id,
+        )
+        .await
+        .unwrap();
 
-//     // logging can take a lot of cu/mem
-//     addin.log_voter_info(&registrar, &voter, 0).await;
+    // logging can take a lot of cu/mem
+    addin.log_voter_info(&registrar, &voter, 0).await;
 
-//     Ok(())
-// }
+    Ok(())
+}
--- programs/voter-stake-registry/tests/test_log_voter_info.rs
+++ programs/voter-stake-registry/tests/test_log_voter_info.rs
@@ -1,161 +1,187 @@
-// use anchor_spl::token::TokenAccount;
-// use program_test::*;
-// use solana_program_test::*;
-// use solana_sdk::{signature::Keypair, signer::Signer, transport::TransportError};
-// use voter_stake_registry::state::{LockupKind, LockupPeriod};
+use anchor_spl::token::TokenAccount;
+use program_test::*;
+use solana_program_test::*;
+use solana_sdk::{signature::Keypair, signer::Signer, transport::TransportError};
+use voter_stake_registry::state::{LockupKind, LockupPeriod};
 
-// mod program_test;
+mod program_test;
 
-// fn deserialize_event<T: anchor_lang::Event>(event: &str) -> Option<T> {
-//     let data = base64::decode(event).ok()?;
-//     if data.len() < 8 || data[0..8] != T::discriminator() {
-//         return None;
-//     }
-//     T::try_from_slice(&data[8..]).ok()
-// }
+fn deserialize_event<T: anchor_lang::Event>(event: &str) -> Option<T> {
+    let data = base64::decode(event).ok()?;
+    if data.len() < 8 || data[0..8] != T::discriminator() {
+        return None;
+    }
+    T::try_from_slice(&data[8..]).ok()
+}
 
-// #[tokio::test]
-// async fn test_print_event() -> Result<(), TransportError> {
-//     println!(
-//         "{:#?}",
-//         deserialize_event::<voter_stake_registry::events::DepositEntryInfo>(
-//             "LP4gbyknBZQAABhzAQAAAAAAGHMBAAAAAAAYcwEAAAAAAAEAAAAAAAAAAAGK6hx3fgEAAAA="
-//         )
-//         .ok_or(())
-//     );
-//     Ok(())
-// }
+#[tokio::test]
+async fn test_print_event() -> Result<(), TransportError> {
+    println!(
+        "{:#?}",
+        deserialize_event::<voter_stake_registry::events::DepositEntryInfo>(
+            "LP4gbyknBZQAABhzAQAAAAAAGHMBAAAAAAAYcwEAAAAAAAEAAAAAAAAAAAGK6hx3fgEAAAA="
+        )
+        .ok_or(())
+    );
+    Ok(())
+}
 
-// #[tokio::test]
-// async fn test_log_voter_info() -> Result<(), TransportError> {
-//     let context = TestContext::new().await;
-//     let addin = &context.addin;
+#[tokio::test]
+async fn test_log_voter_info() -> Result<(), TransportError> {
+    let context = TestContext::new().await;
+    let addin = &context.addin;
 
-//     let payer = &context.users[0].key;
-//     let realm_authority = Keypair::new();
-//     let realm = context
-//         .governance
-//         .create_realm(
-//             "testrealm",
-//             realm_authority.pubkey(),
-//             &context.mints[0],
-//             payer,
-//             &context.addin.program_id,
-//         )
-//         .await;
+    let payer = &context.users[0].key;
+    let realm_authority = Keypair::new();
+    let realm = context
+        .governance
+        .create_realm(
+            "testrealm",
+            realm_authority.pubkey(),
+            &context.mints[0],
+            payer,
+            &context.addin.program_id,
+        )
+        .await;
 
-//     let deposit_authority = &context.users[1].key;
-//     let voter_mngo = context.users[1].token_accounts[0];
-//     let token_owner_record = realm
-//         .create_token_owner_record(deposit_authority.pubkey(), payer)
-//         .await;
+    let deposit_authority = &context.users[1].key;
+    let voter_mngo = context.users[1].token_accounts[0];
+    let token_owner_record = realm
+        .create_token_owner_record(deposit_authority.pubkey(), payer)
+        .await;
 
-//     let fill_authority = Keypair::from_bytes(&context.users[3].key.to_bytes()).unwrap();
-//     let distribution_authority = Keypair::new();
-//     let (registrar, rewards_pool) = context
-//         .addin
-//         .create_registrar(
-//             &realm,
-//             &realm_authority,
-//             payer,
-//             &fill_authority.pubkey(),
-//             &distribution_authority.pubkey(),
-//             &context.rewards.program_id,
-//         )
-//         .await;
-//     let mngo_voting_mint = addin
-//         .configure_voting_mint(
-//             &registrar,
-//             &realm_authority,
-//             payer,
-//             0,
-//             &context.mints[0],
-//             0,
-//             1.0,
-//             1.0,
-//             365 * 24 * 60 * 60,
-//             None,
-//             None,
-//         )
-//         .await;
+    let fill_authority = Keypair::from_bytes(&context.users[3].key.to_bytes()).unwrap();
+    let distribution_authority = Keypair::new();
+    let (registrar, rewards_pool) = context
+        .addin
+        .create_registrar(
+            &realm,
+            &realm_authority,
+            payer,
+            &fill_authority.pubkey(),
+            &distribution_authority.pubkey(),
+            &context.rewards.program_id,
+        )
+        .await;
+    let mngo_voting_mint = addin
+        .configure_voting_mint(
+            &registrar,
+            &realm_authority,
+            payer,
+            0,
+            &context.mints[0],
+            0,
+            1.0,
+            1.0,
+            365 * 24 * 60 * 60,
+            None,
+            None,
+        )
+        .await;
 
-//     // TODO: ??? voter_authority == deposit_authority ???
-//     let voter_authority = deposit_authority;
-//     let deposit_mining = find_deposit_mining_addr(
-//         &voter_authority.pubkey(),
-//         &rewards_pool,
-//         &context.rewards.program_id,
-//     );
+    // TODO: ??? voter_authority == deposit_authority ???
+    let voter_authority = deposit_authority;
+    let deposit_mining = find_deposit_mining_addr(
+        &voter_authority.pubkey(),
+        &rewards_pool,
+        &context.rewards.program_id,
+    );
 
-//     let voter = addin
-//         .create_voter(
-//             &registrar,
-//             &token_owner_record,
-//             voter_authority,
-//             payer,
-//             &rewards_pool,
-//             &deposit_mining,
-//             &context.rewards.program_id,
-//         )
-//         .await;
+    let voter = addin
+        .create_voter(
+            &registrar,
+            &token_owner_record,
+            voter_authority,
+            payer,
+            &rewards_pool,
+            &deposit_mining,
+            &context.rewards.program_id,
+        )
+        .await;
 
-//     addin
-//         .create_deposit_entry(
-//             &registrar,
-//             &voter,
-//             voter_authority,
-//             &mngo_voting_mint,
-//             0,
-//             LockupKind::Constant,
-//             LockupPeriod::OneYear,
-//         )
-//         .await
-//         .unwrap();
-//     addin
-//         .deposit(
-//             &registrar,
-//             &voter,
-//             &mngo_voting_mint,
-//             voter_authority,
-//             voter_mngo,
-//             0,
-//             12000,
-//             &rewards_pool,
-//             &deposit_mining,
-//             &context.rewards.program_id,
-//         )
-//         .await
-//         .unwrap();
+    addin
+        .create_deposit_entry(
+            &registrar,
+            &voter,
+            voter_authority,
+            &mngo_voting_mint,
+            0,
+            LockupKind::None,
+            LockupPeriod::None,
+        )
+        .await
+        .unwrap();
+    addin
+        .create_deposit_entry(
+            &registrar,
+            &voter,
+            voter_authority,
+            &mngo_voting_mint,
+            1,
+            LockupKind::Constant,
+            LockupPeriod::OneYear,
+        )
+        .await
+        .unwrap();
+    addin
+        .deposit(
+            &registrar,
+            &voter,
+            &mngo_voting_mint,
+            voter_authority,
+            voter_mngo,
+            0,
+            12000,
+        )
+        .await
+        .unwrap();
+    addin
+        .lock_tokens(
+            &registrar,
+            &voter,
+            voter_authority,
+            &deposit_mining,
+            &context.rewards.program_id,
+            0,
+            1,
+            12000,
+            mngo_voting_mint.mint.pubkey.unwrap(),
+            realm.realm,
+        )
+        .await?;
 
-//     // advance time to one month ahead
-//     addin
-//         .set_time_offset(&registrar, &realm_authority, 365 * 24 * 60 * 60 / 12)
-//         .await;
-//     context.solana.advance_clock_by_slots(2).await;
+    // advance time to one month ahead
+    addin
+        .set_time_offset(&registrar, &realm_authority, 365 * 24 * 60 * 60 / 12)
+        .await;
+    context.solana.advance_clock_by_slots(2).await;
 
-//     addin.log_voter_info(&registrar, &voter, 0).await;
-//     let data_log = context.solana.program_output().data;
-//     assert_eq!(data_log.len(), 2);
+    addin.log_voter_info(&registrar, &voter, 0).await;
+    let data_log = context.solana.program_output().data;
+    assert_eq!(data_log.len(), 3);
 
-//     let voter_event =
-//         deserialize_event::<voter_stake_registry::events::VoterInfo>(&data_log[0]).unwrap();
-//     assert_eq!(voter_event.voting_power_baseline, 12000);
-//     assert_eq!(voter_event.voting_power, 12000);
+    let voter_event =
+        deserialize_event::<voter_stake_registry::events::VoterInfo>(&data_log[0]).unwrap();
+    assert_eq!(voter_event.voting_power_baseline, 12000);
+    assert_eq!(voter_event.voting_power, 12000);
 
-//     let deposit_event =
-//         deserialize_event::<voter_stake_registry::events::DepositEntryInfo>(&data_log[1]).unwrap();
-//     assert_eq!(deposit_event.deposit_entry_index, 0);
-//     assert_eq!(deposit_event.voting_mint_config_index, 0);
-//     assert_eq!(deposit_event.unlocked, 0);
-//     assert_eq!(deposit_event.voting_power, voter_event.voting_power);
-//     assert_eq!(
-//         deposit_event.voting_power_baseline,
-//         voter_event.voting_power_baseline
-//     );
-//     assert!(deposit_event.locking.is_some());
-//     let locking = deposit_event.locking.unwrap();
-//     assert!(locking.vesting.is_none());
-//     assert_eq!(locking.amount, 12000);
+    let deposit_event =
+        deserialize_event::<voter_stake_registry::events::DepositEntryInfo>(&data_log[1]).unwrap();
+    assert_eq!(deposit_event.deposit_entry_index, 0);
+    assert_eq!(deposit_event.voting_mint_config_index, 0);
+    assert_eq!(deposit_event.unlocked, 0);
 
-//     Ok(())
-// }
+    let deposit_event =
+        deserialize_event::<voter_stake_registry::events::DepositEntryInfo>(&data_log[2]).unwrap();
+    assert_eq!(deposit_event.voting_power, voter_event.voting_power);
+    assert_eq!(
+        deposit_event.voting_power_baseline,
+        voter_event.voting_power_baseline
+    );
+    assert!(deposit_event.locking.is_some());
+    let locking = deposit_event.locking.unwrap();
+    assert!(locking.vesting.is_none());
+    assert_eq!(locking.amount, 12000);
+
+    Ok(())
+}
