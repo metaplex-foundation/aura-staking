213e87120a581e7f8f66fdc1d842df2671847f5f
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-02 15:01:57 +0300

    fmt

--- programs/voter-stake-registry/src/instructions/close_deposit_entry.rs
+++ programs/voter-stake-registry/src/instructions/close_deposit_entry.rs
@@ -23,7 +23,11 @@ pub struct CloseDepositEntry<'info> {
 pub fn close_deposit_entry(ctx: Context<CloseDepositEntry>, deposit_entry_index: u8) -> Result<()> {
     let voter = &mut ctx.accounts.voter.load_mut()?;
     let d = voter.active_deposit_mut(deposit_entry_index)?;
-    require_eq!(d.amount_deposited_native, 0, MplStakingError::VotingTokenNonZero);
+    require_eq!(
+        d.amount_deposited_native,
+        0,
+        MplStakingError::VotingTokenNonZero
+    );
 
     *d = DepositEntry::default();
     d.is_used = false;
